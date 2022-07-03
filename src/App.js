88c9affa4f317b6e20cc0fc601dd27d09af9aaf9
MKASIF <mdasifkhan089@gmail.com>
2022-07-04 01:55:50 +0600

    Done Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser,updateUser,deleteUser } from "./features/Users";
+import { useState } from "react";
 
 function App() {
+  const userList = useSelector((state) => state.users.value);
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [username, setUsrename] = useState("");
+  const [newUsername, setNewUsrename] = useState("");
+console.log(userList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>React Redux CRUD</h1>
+      <div className="addUser">
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          onChange={(e) => setUsrename(e.target.value)}
+          type="text"
+          placeholder="User Name"
+        />
+        <button
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add User
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div key={user.id}>
+              <h1>{user.name}</h1>
+              <h3>{user.username}</h3>
+
+              <input
+                onChange={(e) => setNewUsrename(e.target.value)}
+                type="text"
+                placeholder="New Username"
+              />
+              <button onClick={() => dispatch(updateUser({id:user.id,username: newUsername}))}>
+                Update UserName
+              </button>
+              <button onClick={() => dispatch(deleteUser({ id: user.id }))}>
+                Delete User
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
